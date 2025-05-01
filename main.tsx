5d7781b878ecc9c70a8a9f8c8c5b934cea077217
Halyna Moldovianu <Beorgeos@gmail.com>
2025-05-01 18:59:18 +0200

    1

--- main.tsx
+++ main.tsx
@@ -1,9 +1,9 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import { Toaster } from "react-hot-toast";
+import { StrictMode } from "react";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <App />
     <Toaster />
